d0dd23e3a06aed01204ca3ea900a0dce77ab1191
Alexei Kozhushkov <alexei.kozhushkov@gmail.com>
2022-03-28 10:42:36 +0300

    jwt-sign

--- src/resolvers.js
+++ src/resolvers.js
@@ -73,7 +73,7 @@ const resolvers = {
         const token = jsonwebtoken.sign(
           { id: user.id, email: user.email },
           process.env.JWT_SECRET,
-          { expiresIn: '1d' }
+          { expiresIn: '1y' }
         )
         return {
           token, user
--- src/test.js
+++ src/test.js
@@ -1,4 +1,7 @@
-const { City, Country, Translator } = require('../models')
+const { City, Country, Translator, User } = require('../models')
+const bcrypt = require('bcryptjs')
+const jsonwebtoken = require('jsonwebtoken')
+require('dotenv').config()
 
 async function countryTranslators (_, { isoCode }, { models }) {
   return await models.Country.findOne({
@@ -21,7 +24,7 @@ async function countryTranslators (_, { isoCode }, { models }) {
   })
 }
 
-async function test () {
+async function findTranslatorsByCountry () {
   // const result = await City.findAll({
   //   logging: false,
   //   where: { CountryID: 15 },
@@ -47,6 +50,24 @@ async function test () {
     }]
   })
   console.log(JSON.stringify(result, null, 2))
+}
+
+async function test () {
+  const password = 'Shmassword!!!'
+  const email = 'alexei.kozhushkov@gmail.com'
+
+  const user = await User.findOne({ where: { email } })
+  const isValid = await bcrypt.compare(password, user.password)
+
+  const token = jsonwebtoken.sign(
+    { id: user.id, email: user.email },
+    process.env.JWT_SECRET,
+    { expiresIn: '1y' }
+  )
+
+  console.log({ token, user: user.toJSON() })
+  // const users = await User.findAll()
+  // console.log(JSON.stringify(users, null, 2))
 }
 
 test()
